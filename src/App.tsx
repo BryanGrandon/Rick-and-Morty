f241f1a97330009b017b88123b4bfb6cd5f61f24
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-10-16 12:24:15 -0300

    feat(web):add main-article component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/layout/header";
+import MainArticle from "./components/layout/main-article";
 
 const App = (): JSX.Element => {
   return (
     <>
       <Header />
+      <MainArticle />
     </>
   );
 };
---
+++ src/components/layout/main-article.tsx
@@ -0,0 +1,17 @@
+// import React from "react";
+
+const MainArticle = () => {
+  return (
+    <main className="main-article">
+      <article>
+        <h2>Filter</h2>
+      </article>
+
+      <article>
+        <h2>List</h2>
+      </article>
+    </main>
+  );
+};
+
+export default MainArticle;
